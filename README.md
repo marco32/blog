4bc88bae158bbf64be71667f2eb6114b424c0a74
marco32 <marcofrosi32@gmail.com>
2017-04-18 14:41:22 +0200

    Add Markdown and function delete request server

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 # Commit.
 >* Add request Ajax.
 >* Send and load in blog Okay.
+
+# Commit
+>* Add Markdown.
+>* Add function Delete.
--- js/app.js
+++ js/app.js
@@ -1,56 +1,104 @@
 $(document).ready(function() {
-	load();
-	
+	// Var global
 
 	var blog;
 	var article;
-	console.log("yop")
+	var date= new Date;
+	
+	// My function
 
-	$("#send").click(function(){
-		article={
-			"titre" : $("#Title").val(),
-			"corp" : $("#Text").val(),
+		function save(){
+			$.ajax({ 
+				url:'http://192.168.1.50/json-db', 
+				data: { task: "set",
+				key: "BlogMarco",
+				value: JSON.stringify(article),
+			} 
+		});
+			success:{
+				alert("Mise a jour des données")
+			}
 		}
-		blog.push(article)
-		save();
 
-	})
-	 function AddBlog(tab){
-	 	console.log(blog)
-	 	for (var i = 0; i < tab.length; i++) {
-	 		var add = tab[i]
-	 		$("#titre").append('<div>'+add[i]["titre"]+'</div>');
-	 		$("#art").append('<div>'+add[i]["corp"]+'</div>')
-	 	}
-
-	 }
-
-
-
-
-	function save(){
-		$.ajax({ 
-			url:'http://192.168.1.50/json-db', 
-			data: { task: "set",
-					key: "BlogMarco",
-			 		value: JSON.stringify(blog),
-					} 
-			});
-	}
-
-	function load(){
+		function load(){
 			$.ajax({ 
 				url:'http://192.168.1.50/json-db', 
 				data: { task: 'get',
-						key: 'BlogMarco', 
-						} 
-					})
+				key: 'BlogMarco', 
+			} 
+		})
+			.fail(function(){
+				$("#art").append('<div>'+"Mise à jour en cours veuillez réessayez plus tard"+'</div>')
+				$("#load").append('<div>'+"Mise à jour en cours veuillez réessayez plus tard"+'</div>')
+			
+			})
 			.done(function(data){
 				if(data != null){
 
 					blog=JSON.parse(data);
 					AddBlog(blog);
+					resume();
 				}
 			})
 		}
+		function AddBlog(blog){
+
+			for (var i = 0; i < blog.length; i++) {
+				var add = blog[i]
+				$("#titre").append('<div><a href="'+"#"+[i]+'">'+add.titre+'</a></div>');
+				$("#art").append('<div id="'+[i]+'">'+add["corp"]+'</div>');
+				$("#art").append('<div>'+add["date"]+'</div>');
+			}
+
+		}
+	
+	
+		function resume(){
+	
+		$("#Affcontenue").html(" ");
+		for (var i = 0; i < blog.length; i++) {
+		
+			var baff = blog[i] 
+			i
+			var ligne = $('<tr/>').data("ID",i);
+
+			$('<td/>').html(i).appendTo(ligne);
+			$("<td>"+ baff["titre"] +"</td>").appendTo(ligne);
+			$("<td>" + baff["date"]+ "</td>" ).appendTo(ligne);
+			$("<th>" + baff["_id"]+ "</th>" ).appendTo(ligne);
+			$("#Affcontenue").append( ligne ); 				
+		}	
+	}
+	// Appel function
+	load();
+
+	// Click function
+	$("#Affcontenue").delegate('th', 'click', function(){
+		 var supp = $(this).html()
+	 	$.ajax({ 
+	 		url:'http://192.168.1.50/json-db', 
+	 		data: { task: "delete",
+	 		key: "BlogMarco",
+	 		_id: supp,
+	 	} 
+	 });	 
+		load();
+	})
+
+	$("#send").click(function(){
+		article={
+			"titre" : $("#Title").val(),
+			"corp" : $("#Text").html(),
+			"date" : date,
+		}
+		save();
+		$("#Title").val("");
+		$("#Text").html("");
+		$("#Textblog").val("");
+	})
+	$("#Textblog").keyup(function(){
+		var mark = markdown.toHTML($(this).val());	
+		$("#Text").html(mark);
+	})
+
 });
